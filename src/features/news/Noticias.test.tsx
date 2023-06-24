35d2fd0237ed951d3d887d352104805330afa901
Serafin Quesada <serafinkocho@gmail.com>
2023-06-23 22:04:58 -0300

    primeros test en Noticias

--- src/features/news/Noticias.test.tsx
+++ src/features/news/Noticias.test.tsx
@@ -1,8 +1,28 @@
 import {screen } from "@testing-library/react";
 import {render} from "../../test-utils"
+import Noticias from "./Noticias";
+import userEvent from "@testing-library/user-event";
+
 
 describe("NoticiasComponent", () => {
-    
+
+    it("Should render three buttons", async () => {
+        render(<Noticias/>)
+        
+        const buttons = await screen.findAllByRole('button');
+        expect(buttons.length).toBe(3);
+    });
+    it("Should render button Suscribite when prop esPremium is true", async () => {
+        render(<Noticias/>)
+        
+        const buttons = await screen.findAllByRole('button');
+        const buttonVerMas3 = buttons[buttons.length - 1];
+
+        userEvent.click(buttonVerMas3)
+
+        const buttonSuscribite = await screen.findByRole('button'); 
+        expect(buttonSuscribite).toBeInTheDocument();
+    })
 })
 
 export {};
